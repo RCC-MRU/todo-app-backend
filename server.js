497ddc34b504e180b13b8982545b9e8ced0d24ea
Sanchit <sanchit02@outlook.com>
2021-08-12 20:11:13 +0530

    folder structure created and files added

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+const cors = require('cors')
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// database check
+const db = require("./database/db");
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("DB connection");
+});
+
+const mainRouter = require("./routes/main");
+app.use('/', mainRouter);
+
+
+// if you enter any wrong route, request automatically redirect to this because it has *
+app.get("*", function (req, res) {
+  res.status(200);
+  res.json({
+    message: "This is the root route",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
